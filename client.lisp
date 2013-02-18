129b02343be3bbfa23dd87289918c9c170c83f6f
Hans Huebner <hans.huebner@gmail.com>
2013-02-18 22:45:54 +0100

    refactor

--- client.lisp
+++ client.lisp
@@ -1,84 +1,119 @@
-                                        ; Lisp client for BaseX.
-                                        ; Works with BaseX 6.1
-                                        ;
-                                        ; Documentation: http://docs.basex.org/wiki/Clients
-                                        ; 
-                                        ; (C) Andy Chambers, Formedix Ltd 2010, BSD License
+;; Lisp client for BaseX.
+;; Works with BaseX 6.1
+;;
+;; Documentation: http://docs.basex.org/wiki/Clients
+;; 
+;; (C) Andy Chambers, Formedix Ltd 2010, BSD License
 
 (defpackage :basex
-  (:use :cl :usocket)
-  (:export :session
-   :execute
-           :close-session
-   :info
-           :result))
+  (:use :cl)
+  (:export #:connect
+           #:execute
+           #:disconnect
+           #:*db-host*
+           #:*db-port*
+           #:*db-user*
+           #:*db-password*
+           #:login-failed
+           #:host
+           #:port
+           #:user
+           #:execute-error
+           #:query
+           #:result
+           #:info))
 
 (in-package :basex)
 
 (defconstant +default-read-buffer-size+ 65536)
 
+(defparameter *db-host* "localhost")
+(defparameter *db-port* 1984)
+(defparameter *db-user* "admin")
+(defparameter *db-password* "admin")
+
+(define-condition login-failed (error)
+  ((host :initarg :host :reader host)
+   (port :initarg :port :reader port)
+   (user :initarg :user :reader user))
+  (:report (lambda (c stream)
+             (format stream "could not log in to BaseX on ~A:~A (user ~S)"
+                     (host c) (port c) (user c)))))
+
+(define-condition execute-error (error)
+  ((query :initarg :query :reader query)
+   (result :initarg :result :reader result)
+   (info :initarg :info :reader info))
+  (:report (lambda (c stream)
+             (format stream "query execution failed~%query ~A~%result ~A~%info ~A"
+                     (query c) (result c) (info c)))))
+
 (defclass session ()
-  ((host :initarg :host :initform "localhost")
-   (port :initarg :port :initform 1984)
-   (user :initarg :user :initform "admin")
-   (pw :initarg :pw :initform "admin")
-   (sock :initform nil)
-   (result :initform nil :accessor result)
-   (info :initform nil :accessor info)))
+  ((socket :initarg :socket :reader socket)))
 
-(defmethod initialize-instance :after ((self session) &key)
-  (with-slots (host port user pw sock) self
-    (setf sock (socket-connect host port :element-type '(unsigned-byte 8)))
-    (unless (hand-shake self)
-      (error "Could not initiate connection"))))
+(defun connect (&key
+                (host *db-host*)
+                (port *db-port*)
+                (user *db-user*)
+                (password *db-password*))
+  (let ((socket (usocket:socket-connect host port :element-type '(unsigned-byte 8))))
+    (handler-bind
+        ((login-failed (lambda (e)
+                         (setf (slot-value e 'host) host
+                               (slot-value e 'port) port
+                               (slot-value e 'user) user)))
+         (error (lambda (e)
+                  (declare (ignore e))
+                  (usocket:socket-close socket))))
+      (hand-shake socket user password)
+      (make-instance 'session :socket socket))))
 
-(defun hand-shake (session)
+(defmethod disconnect ((self session))
+  (with-slots (socket) self
+    (ignore-errors
+     (write-null-terminated "exit" socket)
+     (usocket:socket-close socket))))
+
+(defun hand-shake (socket user password)
   (declare (optimize debug))
   (labels ((md5 (str)
              (string-downcase (with-output-to-string (s)
-                                (loop for hex across
-                                              (md5:md5sum-string str)
+                                (loop for hex across (md5:md5sum-string str)
                                       do (format s "~2,'0x" hex)))))
-           (auth-token (pw timestamp)
+           (auth-token (password timestamp)
              (md5 (format nil "~a~a"
-                          (md5 pw) timestamp))))
+                          (md5 password) timestamp))))
 
-    (with-slots (user pw sock) session
-      (let* ((ts (read-null-terminated (socket-stream sock)))
-             (auth (auth-token pw ts)))
-        (write-null-terminated user (socket-stream sock))
-        (write-null-terminated auth (socket-stream sock))
-        (force-output (socket-stream sock))
-        (eq 0 (read-byte (socket-stream sock)))))))
+    (let* ((ts (read-null-terminated socket))
+           (auth (auth-token password ts)))
+      (write-null-terminated user socket)
+      (write-null-terminated auth socket)
+      (force-output (usocket:socket-stream socket))
+      (unless (zerop (read-byte (usocket:socket-stream socket)))
+        (error 'login-failed :user user)))))
 
-(defun read-null-terminated (in)
+(defun read-null-terminated (socket)
   (let ((buf (make-array +default-read-buffer-size+ :adjustable t :fill-pointer 0 :element-type '(unsigned-byte 8))))
-    (loop for byte = (read-byte in)
+    (loop for byte = (read-byte (usocket:socket-stream socket))
           until (zerop byte)
           do (vector-push-extend byte buf))
     (babel:octets-to-string buf :encoding :utf-8)))
 
-(defun write-null-terminated (string out)
+(defun write-null-terminated (string socket)
   (loop for byte across (babel:string-to-octets string :encoding :utf-8)
-        do (write-byte byte out))
-  (write-byte 0 out))
+        do (write-byte byte (usocket:socket-stream socket)))
+  (write-byte 0 (usocket:socket-stream socket)))
 
 (defmethod execute ((self session) query)
-  (with-slots (sock) self
-    (let ((stream (socket-stream sock)))
-      (write-null-terminated query stream)
-      (force-output stream)
-      (setf (result self) (read-null-terminated stream)
-            (info self) (read-null-terminated stream))
-      (eq 0 (read-byte (socket-stream sock))))))
+  (with-slots (socket) self
+    (write-null-terminated query socket)
+    (force-output (usocket:socket-stream socket))
+    (let* ((result (read-null-terminated socket))
+           (info (read-null-terminated socket)))
+      (unless (eql 0 (read-byte (usocket:socket-stream socket)))
+        (error 'execute-error :query query
+                              :result result
+                              :info info))
+      (values result info))))
 
-(defmethod open-session ((self session))
-  (unwind-protect
-       (unless (hand-shake self)
-         (error "Could not open session"))
-    (close-session self)))
 
-(defmethod close-session ((self session))
-  (with-slots (sock) self
-    (write-null-terminated "exit" (socket-stream sock))
-    (socket-close sock)))
